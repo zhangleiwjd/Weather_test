0544a47b47fd8fdcf0f99bf571360302c77f5c05
zhangleiwjd <zhangleiwjd@gmail.com>
2016-08-22 10:39:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/zhanglei/weather_test/model/City.java
@@ -0,0 +1,43 @@
+package com.example.zhanglei.weather_test.model;
+
+/**
+ * Created by zhanglei on 2016-08-22.
+ */
+public class City {
+    private int id;
+    private String cityName;
+    private String cityCode;
+    private int provinceId;
+
+    public String getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(String cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
---
+++ app/src/main/java/com/example/zhanglei/weather_test/model/Province.java
@@ -0,0 +1,34 @@
+package com.example.zhanglei.weather_test.model;
+
+/**
+ * Created by zhanglei on 2016-08-22.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
