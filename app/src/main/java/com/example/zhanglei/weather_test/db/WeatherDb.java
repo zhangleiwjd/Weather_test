4c5df49e4e5f534acf49971bcd55d15fbd3073de
zhangleiwjd <zhangleiwjd@gmail.com>
2016-08-22 10:58:48 +0800

    Rejected commit: Default

--- app/src/main/java/com/example/zhanglei/weather_test/db/WeatherDb.java
+++ app/src/main/java/com/example/zhanglei/weather_test/db/WeatherDb.java
@@ -1,7 +1,14 @@
 package com.example.zhanglei.weather_test.db;
 
+import android.database.sqlite.SQLiteDatabase;
+
 /**
  * Created by zhanglei on 2016-08-22.
  */
 public class WeatherDb {
+    public static final String DB_NAME = "weather";
+    public static final int VERSION = 1;
+    public static WeatherDb weatherDb;
+    private SQLiteDatabase db;
+
 }
